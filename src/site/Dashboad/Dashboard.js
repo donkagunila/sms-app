3348c5f50e2b5ad84fa334d8ec45d93673c9ba6a
Donald Kagunila <donald@smartcodes.co.tz>
2021-05-18 20:17:44 +0300

    add white cards

--- src/site/Dashboad/Dashboard.js
+++ src/site/Dashboad/Dashboard.js
@@ -1,34 +1,34 @@
 import React from 'react';
-import { FiMessageCircle, FiPieChart, FiRadio } from 'react-icons/fi';
+import { FiCreditCard, FiMessageCircle, FiPieChart, FiRadio } from 'react-icons/fi';
 
 
 export default function Dashboard() {
   return (
     <div className="main-content">
       
-      <div class="header">
-        <div class="container-fluid">
+      <div className="header">
+        <div className="container-fluid">
 
           
-          <div class="header-body">
-            <div class="row align-items-end">
-              <div class="col">
+          <div className="header-body">
+            <div className="row align-items-end">
+              <div className="col">
 
                 
-                <h6 class="header-pretitle">
+                <h6 className="header-pretitle">
                   Overview
                 </h6>
 
                
-                <h1 class="header-title">
+                <h1 className="header-title">
                   Dashboard
                 </h1>
 
               </div>
-              <div class="col-auto">
+              <div className="col-auto">
 
                
-                <a href="#!" class="btn btn-primary lift">
+                <a href="#!" className="btn btn-primary lift">
                   Schedule  Campaign
                 </a>
 
@@ -39,39 +39,35 @@ export default function Dashboard() {
         </div>
       </div> 
     
-      <div class="container-fluid">
-        <div class="row">
-          <div class="col-12 col-lg-6 col-xl">
+      <div className="container-fluid">
+        <div className="row">
+         
+        <div className="col-12 col-lg-6 col-xl">
 
 
-    
-            <div class="card bg-dark">
-              <div class="card-body">
-                <div class="row align-items-center">
-                  <div class="col">
+            <div className="card">
+              <div className="card-body">
+                <div className="row align-items-center">
+                  <div className="col">
 
-                   
-                    <h6 class="text-uppercase text-white mb-2">
+                  
+                    <h6 className="text-uppercase text-muted mb-2">
                       Campaigns
                     </h6>
 
-                    <span class="h2 mb-0 text-white">
-                      24
+                  
+                    <span className="h2 mb-0">
+                      45
                     </span>
 
+                  
                     
-                  
-
-                   
-                    <span class="badge badge-soft-light mt-n1 text-dark ml-2">
-                      +3.5%
-                    </span>
                   </div>
-                  <div class="col-auto">
+                  <div className="col-auto">
 
                   
-                    <span class="h2 fe fe-dollar-sign text-muted mb-0">
-                      <FiRadio className="text-white"/>
+                    <span className="h2 fe fe-dollar-sign text-muted mb-0">
+                      <FiRadio />
                     </span>
 
                   </div>
@@ -79,126 +75,124 @@ export default function Dashboard() {
               </div>
             </div>
 
+            </div> 
+        
+           
+        <div className="col-12 col-lg-6 col-xl">
 
+
+          <div className="card">
+            <div className="card-body">
+              <div className="row align-items-center">
+                <div className="col">
+
+                
+                  <h6 className="text-uppercase text-muted mb-2">
+                    Total Cost
+                  </h6>
+
+                
+                  <span className="h2 mb-0">
+                     24,500 <small>tsh</small>
+                  </span>
+
+                
+                  <span className="badge badge-soft-success mt-n1 ml-2">
+                    +3.5%
+                  </span>
+                </div>
+                <div className="col-auto">
+
+                
+                  <span className="h2 fe fe-dollar-sign text-muted mb-0">
+                    <FiCreditCard />
+                  </span>
+
+                </div>
+              </div> 
+            </div>
           </div>
 
-
-          <div class="col-12 col-lg-6 col-xl">
-
-
-    
-          <div class="card bg-warning">
-  <div class="card-body">
-    <div class="row align-items-center">
-      <div class="col">
-
-       
-        <h6 class="text-uppercase text-white mb-2">
-          SMS OUT
-        </h6>
-
-        
-        <span class="h2 mb-0 text-white">
-          24,720,123
-        </span>
-
-       
-       
-      </div>
-      <div class="col-auto">
-
-      
-        <span class="h2 fe fe-dollar-sign text-muted mb-0">
-          <FiMessageCircle className="text-white"/>
-        </span>
-
-      </div>
-    </div> 
-  </div>
-</div>
+        </div> 
 
 
-</div>
+        <div className="col-12 col-lg-6 col-xl">
 
 
-<div class="col-12 col-lg-6 col-xl">
+          <div className="card">
+            <div className="card-body">
+              <div className="row align-items-center">
+                <div className="col">
+
+                
+                  <h6 className="text-uppercase text-muted mb-2">
+                    SMS IN
+                  </h6>
+
+                
+                  <span className="h2 mb-0">
+                    224,500
+                  </span>
+
+                
+                  <span className="badge badge-soft-success mt-n1 ml-2">
+                    +7.5%
+                  </span>
+                </div>
+                <div className="col-auto">
+
+                
+                  <span className="h2 fe fe-dollar-sign text-muted mb-0">
+                    <FiMessageCircle />
+                  </span>
+
+                </div>
+              </div> 
+            </div>
+          </div>
+
+          </div> 
 
 
-    
-<div class="card bg-info">
-  <div class="card-body">
-    <div class="row align-items-center">
-      <div class="col">
-
-       
-        <h6 class="text-uppercase text-white mb-2">
-          Total income
-        </h6>
-
-        
-        <span class="h2 mb-0 text-white">
-          24,720 <small>(Tsh)</small>
-        </span>
-
-       
-       
-      </div>
-      <div class="col-auto">
-
-      
-        <span class="h2 fe fe-dollar-sign text-muted mb-0">
-          <FiPieChart className="text-white"/>
-        </span>
-
-      </div>
-    </div> 
-  </div>
-</div>
+        <div className="col-12 col-lg-6 col-xl">
 
 
+          <div className="card">
+            <div className="card-body">
+              <div className="row align-items-center">
+                <div className="col">
 
-</div>
+                
+                  <h6 className="text-uppercase text-muted mb-2">
+                    SMS OUT
+                  </h6>
 
-<div class="col-12 col-lg-6 col-xl">
+                
+                  <span className="h2 mb-0">
+                    47,561
+                  </span>
 
+                
+                  <span className="badge badge-soft-danger mt-n1 ml-2">
+                    -3.5%
+                  </span>
+                </div>
+                <div className="col-auto">
 
-    
-<div class="card bg-success">
-  <div class="card-body">
-    <div class="row align-items-center">
-      <div class="col">
+                
+                  <span className="h2 fe fe-dollar-sign text-muted mb-0">
+                  <FiMessageCircle />
+                  </span>
 
-       
-        <h6 class="text-uppercase text-white mb-2">
-          Total income
-        </h6>
+                </div>
+              </div> 
+            </div>
+          </div>
 
-        
-        <span class="h2 mb-0 text-white">
-          24,720 <small>(Tsh)</small>
-        </span>
-
-       
-       
-      </div>
-      <div class="col-auto">
-
-      
-        <span class="h2 fe fe-dollar-sign text-muted mb-0">
-          <FiPieChart className="text-white"/>
-        </span>
-
-      </div>
-    </div> 
-  </div>
-</div>
+          </div> 
 
 
-</div>
 
-
-       
-        
         </div>
       </div>
     </div>
